f8b365b65f1a79ebac5fa5979fa7036d815c9821
nandapputtrrii <nandappuuttrrii12@gmail.com>
2025-03-13 04:17:01 +0700

    Animal.java

--- Animal.java
+++ Animal.java
@@ -1,50 +1,53 @@
 // Animal.java
 public class Animal {
-    // Attributes (fields)
+    // atribut (fields)
     private String name;
     private String type;
     private String sound;
 
-    // Constructor
+    // konstruktor untuk inisialisasi objek Animal
     public Animal(String name, String type, String sound) {
         this.name = name;
         this.type = type;
         this.sound = sound;
     }
 
-    // Getter methods (optional but common practice in Java)
+    // metode getter untuk mengambil nama hewan
     public String getName() {
         return name;
     }
 
+    // metode getter untuk mengambil tipe hewan
     public String getType() {
         return type;
     }
 
+    // metode getter untuk mengambil suara hewan
     public String getSound() {
         return sound;
     }
 
-    // Display information about the animal
+    // menampilkan informasi tentang hewan
     public void displayInfo() {
-        System.out.println("Name: " + name);
-        System.out.println("Type: " + type);
-        System.out.println("Sound: " + sound);
-        System.out.println(); // For better readability
+        System.out.println("Name: " + name);  // menampilkan nama hewan
+        System.out.println("Type: " + type);  // menampilkan tipe hewan
+        System.out.println("Sound: " + sound); // menampilkan suara hewan
+        System.out.println(); // untuk jarak yang lebih baik antar informasi
     }
 }
 
+// Main.java
 class Main {
     public static void main(String[] args) {
-        // Create 2 animal objects
+        // membuat dua objek Animal
         Animal animal1 = new Animal("Cat", "Mammal", "Nyann~~");
         Animal animal2 = new Animal("Dog", "Mammal", "Woof-Woof!!");
 
-        // Display information for both animals
+        // menampilkan informasi untuk kedua objek animal
         System.out.println("Animal 1 Info:");
-        animal1.displayInfo();
+        animal1.displayInfo(); // menampilkan info untuk animal1
 
         System.out.println("Animal 2 Info:");
-        animal2.displayInfo();
+        animal2.displayInfo(); // menampilkan info untuk animal2
     }
 }
